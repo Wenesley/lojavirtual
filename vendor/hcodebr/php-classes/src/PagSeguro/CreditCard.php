06906c30aad6f241f303ad473ea0b198c1ab57d1
Wenesley <wenesleysantos@gmail.com>
2020-06-07 02:03:30 -0300

    Classe creditCard

--- vendor/hcodebr/php-classes/src/PagSeguro/CreditCard.php
+++ vendor/hcodebr/php-classes/src/PagSeguro/CreditCard.php
@@ -8,6 +8,44 @@
 		private $installment;
 		private $holder;
 		private $billingAddress;
+
+		public function __construct(Stirng $token, Installment $installment, Holder $holder, Address $billingAddress)
+		{
+
+			if(!$token)
+			{
+				throw new Exception("Informe o token do cartão de crédito.");
+				
+			}
+
+			$this->token = $token;
+			$this->installment = $installment;
+			$this->holder = $holder;
+			$this->billingAddress = $billingAddress;
+		}
+
+		public function getDOMElement():DOMElement
+		{
+
+			$dom = new DOMDocument();
+
+			$creditCard = $dom->createElement("creditCard");
+			$creditCard = $dom->appendChild($creditCard);
+			
+			$installment = $this->installment->getDOMElement();
+			$installment = $dom->importNode($installment, true);
+			$installment = $creditCard->appendChild($installment);
+
+			$holder = $this->holder->getDOMElement();
+			$holder = $dom->importNode($holder, true);
+			$holder = $creditCard->appendChild($holder);
+
+			$billingAddress = $this->billingAddress->getDOMElement("billingAddress");
+			$billingAddress = $dom->importNode($billingAddress, true);
+			$billingAddress = $creditCard->appendChild($billingAddress);			
+
+			return $creditCard;
+		}
 		
 	}
 
